de9ec7916e46c40795f1a96e01b2e6ecb05abb2a
Kartavya Sharma <kartavya@berkeley.edu>
2023-10-20 20:07:43 -0700

    Selenium WebDriver is working with live urls

--- scripts/services/scraper.py
+++ scripts/services/scraper.py
@@ -1,7 +1,12 @@
+from typing import Any
 import pandas
 
 import scripts.services.auth as auth
 import constants.constants as constants
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 class ScraperBuilder:
@@ -9,7 +14,22 @@ class ScraperBuilder:
 
 
 class ScraperEngine:
-    pass
+
+    def __init__(self, options: Options, driver: webdriver.Chrome = None):
+        self.options = options
+        self.driver = driver
+        self.service = Service(
+            executable_path=constants.CHROME_DRIVER_EXECUTABLE)
+
+    def __getattribute__(self, __name: str) -> Any:
+        if __name != "service":
+            return super().__getattribute__(__name)
+
+    def create_driver(self) -> None:
+        if not self.driver:
+            self.driver = webdriver.Chrome(
+                options=self.options, service=self.service
+            )
 
 
 class ConfigurationBuilder:
@@ -17,4 +37,8 @@ class ConfigurationBuilder:
 
 
 def main(url: str) -> pandas.DataFrame:
-    print(constants.CHROME_DRIVER_EXECUTABLE)
+    scraper_opts = Options()
+    scraper_opts.headless = False
+    scraper_engine = ScraperEngine(scraper_opts)
+    scraper_engine.create_driver()
+    scraper_engine.driver.get(url)
