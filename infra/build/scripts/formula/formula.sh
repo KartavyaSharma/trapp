a308fea37d9b1f1a6005a1a694894c4090147afd
Kartavya Sharma <kartavya@berkeley.edu>
2023-12-30 01:21:17 -0800

    The following files were changed:\n infra/build/scripts/formula/formula.sh\n

--- infra/build/scripts/formula/formula.sh
+++ infra/build/scripts/formula/formula.sh
@@ -7,3 +7,23 @@ if [ ! -f "./infra/build/scripts/tarball.sh" ]; then
     exit 1
 fi
 
+# Run the tarball script and capture the last line of output
+output=$(./infra/build/scripts/tarball.sh)
+sha=$(echo "$output" | tail -n 1)
+
+# Check if the length of the last line is equal to 256
+if [ ! ${#sha} -eq 256 ]; then
+    echo "Error: The length of the last line of output is not equal to 256."
+    exit 1
+fi
+
+# Run the codegen script and give it the sha as an argument
+./infra/build/scripts/codegen.sh --sha="$sha"
+
+# Check if the codegen script ran successfully
+if [ -f "./infra/build/formula/out/trapp.rb" ]; then
+    echo "Formula generated successfully."
+else
+    echo "Error: Formula generation failed."
+    exit 1
+fi
