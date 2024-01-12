b3e9c638207eb11795a66b61647b0dfebc6d3af0
Kartavya Sharma <kartavya@berkeley.edu>
2024-01-12 06:01:12 -0800

    The following files were changed:\n start.sh\n

--- start.sh
+++ start.sh
@@ -181,13 +181,13 @@ clean() {
     echo -e "Removing .cache\nRemoving pid\nRemoving logs\nRemoving TRAPP-DAEMON.pid\nRemoving bkp.out"
     rip $TRAPP_HOME/.cache $TRAPP_HOME/bkp/pid $TRAPP_HOME/bkp/logs $TRAPP_HOME/bkp/bkp.out bkp
     echo "Removing preview files..."
-    # Delete any preview files with the .preview extension
-    echo "The following files will be deleted:"
     # Check if there are any preview files
     preview_files=$(find $TRAPP_HOME -type f -name "*.preview" -print)
     if [[ "$preview_files" == "" ]]; then
         echo "No preview files found!"
     else
+        # Delete any preview files with the .preview extension
+        echo "The following files will be deleted:"
         echo "$preview_files"
         # Ask user if they want to delete preview files
         read -p "Are you sure you want to delete these files? (y/n) " -n 1 -r
